273f873b8568f39e93a510874438e8edc57ff7ae
Dan Boots <thestereojackers@gmail.com>
2020-06-30 11:28:03 +0000

    Addition of style.css fonts so increase readability

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-alert('Alreet Dan');
+// This is a single line comment // 
+// Single Line comments consist of two forward slashes! to end the single line comment end with two slashes like this //
+
+// A multi line comment is as such /* */ // 
+
+/* For 
+example
+i have multiple 
+lines of comments to be ignored 
+and i end with */
+
+
+// standard alert box //
+// alert('Alreet Dan'); // 
+
+// console.log('Hello World!') 
+
+
+/* 
+ This variable is called
+ "a" and has a value set to
+ 10 
+*/
+
+let a = 10;
+a;
+
+// This let a = 10; will log 10 to the console. 
+
+//A quicker way to assign multiple variables for instance //
+
+let a = 10; b = 15; c = 20;
+
+console.log(a,b,c)
+
+
+/*  
+ variable a and 
+ variable A are two
+ different variables because
+ Javascript differentiate 
+ lowercased and upercased 
+ names
+*/
+
+var a = 5;
+var A = 7;
+
+/*
+Variable totalSize is camelCased. 
+
+*/
+var totalSize = 100;
+
+console.log("a is set to: " + a);
+console.log("But A is set to: " + A);
+console.log(totalSize);
+
+// long variable name //
+
+let longVariableName = "This variable has a long name with multiple words, so it's camelCased.";
+// This will log your variable to the console:
+
+console.log(longVariableName);
